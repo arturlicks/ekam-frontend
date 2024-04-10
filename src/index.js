3beeeb2124587b6eacff54870a1db8fc02e02a58
Artur <arturlicks@gmail.com>
2024-04-10 00:27:14 -0400

    Router-dom config in the nav section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './pages/Home';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import { Calendar } from './pages/Calendar';
 import "./styles.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calendar" element={<Calendar />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );

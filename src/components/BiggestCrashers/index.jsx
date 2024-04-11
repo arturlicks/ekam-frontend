0b60b5877c1c058dfb7dd1cbac701ffac3988c90
Artur <arturlicks@gmail.com>
2024-04-11 13:53:37 -0400

    Added data to the Biggest Crashers table

--- src/components/BiggestCrashers/index.jsx
+++ src/components/BiggestCrashers/index.jsx
@@ -1,39 +1,49 @@
 import "./styles.css"
+import { useState, useEffect } from "react";
 
 function BiggestCrashers() {
-  return (
-    <div>
-        <table>
-            <thead>
-            <tr>
-                <th colSpan="3">Campeonato de batedores</th>
-            </tr>
-            <tr>
-                <th>Posição</th>
-                <th>Piloto</th>
-                <th>Incidentes</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>1</td>
-                <td>João</td>
-                <td>100</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Maria</td>
-                <td>90</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Paulo</td>
-                <td>80</td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
-  )
+
+    const [crashersTable, setCrashersTable] = useState('');
+
+    const handleGetData = async () => {
+        const response = await fetch('http://localhost:8080/parsejson');
+        const data = await response.json();
+  
+        setCrashersTable(data);
+    }
+
+    useEffect(() => {
+        handleGetData();
+    }, []);
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th colSpan="3">Campeonato de batedores</th>
+                    </tr>
+                    <tr>
+                        <th>Posição</th>
+                        <th>Piloto</th>
+                        <th>Incidentes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {crashersTable['race_result'] &&
+                        crashersTable['race_result']
+                            .sort((a, b) => b.incidents - a.incidents) // Sort the array by incidents in descending order
+                            .map((entry, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{entry.display_name}</td>
+                                    <td>{entry.incidents}</td>
+                                </tr>
+                            ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export { BiggestCrashers } 
+export { BiggestCrashers }
